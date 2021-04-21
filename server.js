2ffaa8dcf83ff04e16633eddde98b708eeae7948
David <DavidLegrand@users.noreply.github.com>
2021-04-21 15:36:14 +0200

    Added Waiting URL

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+
+app.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "build", "waiting.html"));
+});
+
 app.use(express.static(path.join(__dirname, "build")));
 
-
-app.get("/test-landing", function (req, res) {
-  console.log('there')
+app.get("/test", function (req, res, next) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.get("/*", function (req, res) {
-  console.log('here')
-  res.sendFile(path.join(__dirname, "build", "static", "media", "waiting.jpg"));
-});
 
-const port = process.env.PORT || 80;
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`running on port ${port}`));

73a6df79125bf0da76de6fad4f061a6ec44dd860
David <DavidLegrand@users.noreply.github.com>
2021-04-23 12:48:41 +0200

    Added Mailing

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+
 const express = require("express");
 const path = require("path");
 const app = express();
+const mailer = require("./mailer")
+const cors = require("cors")
 
-
+app.use(express.static(path.join(__dirname, "waiting")));
 app.get("/", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "build", "waiting.html"));
+  res.sendFile(path.join(__dirname, "waiting", "index.html"));
 });
+
+app.use(express.json());
+app.use(cors())
+app.use("/api/sendmail", (req, res) => {
+  mailer(req.body)
+})
 
 app.use(express.static(path.join(__dirname, "build")));
 
@@ -14,5 +23,6 @@ app.get("/test", function (req, res, next) {
 });
 
 
-const port = process.env.PORT || 8080;
+
+const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`running on port ${port}`));
